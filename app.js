ab0ed911045e60b7da5975bb34fdd04937cccc0d
Owen Scott <owen.m.scott@gmail.com>
2014-07-25 19:48:10 -0400

    works for single-chunk files

--- app.js
+++ app.js
@@ -16,98 +16,169 @@ var flatJsonToNested = require('./src/flatJsonToNested'),
 	through = require('through'),
 	WritableStream = require('stream').Writable;
 
-var TABLE_NAME = 'exampleTable';
 
-module.exports = function csv2xml (dataOrConf, conf, callback) {
 
-	csvData = csvData || {};
-	tables = tables || {};
+var fs = require('fs'), 
+	WritableStream = require('stream').Writable,
+	csvParse = require('csv-parse'),
+	flatJsonToNested = require('./src/flatJsonToNested'),
+	json2xml = require('js2xmlparser'),
+	through = require('through'),
+	_ = require('lodash');
 
-	//lazy assumption that if callback is present other args are present
-	if ((callback) {
-			//TODO: implement with callback
-			//need to pipe the result into the other streams, aggregate it all, and then send it to the callback
-	}
 
-	else {
-		(function() {
+module.exports = function Csv2Xml(conf) {
 
-			var primaryKey = tables[TABLE_NAME].primaryKey,
-				parser = csvParse({columns:true}),
-				group,
-				mapFields,
-				toXML;
+	return through(function write (data) {
 
-				mapFields = WritableStream():
+		var self = this,
+			parser = csvParse({columns:true}),
+			group = WritableStream(),
+			mapFields = WritableStream(),
+			toXML = WritableStream(),
+			splitter = WritableStream();
 
-				mapFields._write = function(chunk, enc, next) {
-					this.queue(flatJsonToNested(data, tables.exampleTable));
-				})
+		//converts json to XML and writes to the parent function's queue (to be piped to wherever by the user)
+		toXML._write = function(chunk, enc, next) {
+			data = JSON.parse(chunk.toString());
+			self.queue(json2xml('iati-activity', data))
+			self.queue('\n');	
+			next();	
+		}
 
-				group = WritableStream();
+		//takes flat JSON and converts to nested JSON according to the user-supplied configuration and writes to 'toXML'
+		mapFields._write = function(chunk, enc, next) {
+			var data = JSON.parse(chunk.toString());
+			toXML.write(JSON.stringify(flatJsonToNested(data, conf)), 'utf8')
+			next();
+		}
+
+		//takes the raw CSV stream and groups it into batches by primary key nad writes to 'mapFields'
+		group._write = function(chunk, enc, next) {
+
+			var data = JSON.parse(chunk.toString());
+
+			this.backlog = this.backlog || [];
+
+			if (data[conf.primaryKey] !== this.currentRecord && this.backlog.length) {
+				this.currentRecord = data[conf.primaryKey];
+				// console.log('Starting ' + this.currentRecord);
+				// console.log('==================================')
+				mapFields.write(JSON.stringify(_.clone(this.backlog)), 'utf8');
+				this.backlog = [];
+			}
+			else {
+				this.backlog.push(data);
+			}
+			next();
+
+		};
+
+		//parses the CSV text into flat json objects (one per row) and then writes to 'group'
+		parser.on('data', function(data) {
+			// console.log('parser');
+			group.write(JSON.stringify(data), 'utf8');
+		})
+
+
+		parser.write(data);
+
+	})
+}
+
+
+
+// module.exports = function csv2xml (dataOrConf, conf, callback) {
+
+// 	csvData = csvData || {};
+// 	tables = tables || {};
+
+// 	//lazy assumption that if callback is present other args are present
+// 	if ((callback) {
+// 			//TODO: implement with callback
+// 			//need to pipe the result into the other streams, aggregate it all, and then send it to the callback
+// 	}
+
+// 	//return a stream
+// 	else {
+
+// 		(function() {
+
+// 			var primaryKey = tables[TABLE_NAME].primaryKey,
+// 				parser = csvParse({columns:true}),
+// 				group,
+// 				mapFields,
+// 				toXML;
+
+// 				mapFields = WritableStream():
+
+// 				mapFields._write = function(chunk, enc, next) {
+// 					this.queue(flatJsonToNested(data, tables.exampleTable));
+// 				})
+
+// 				group = WritableStream();
 					
-				group._write = function(chunk, enc, next) {
+// 				group._write = function(chunk, enc, next) {
 
-					this.backlog = this.backlog || [];
+// 					this.backlog = this.backlog || [];
 
-					if (data[primaryKey] !== this.currentRecord) {
-						this.currentRecord = data[primaryKey];
-						console.log('Starting ' + this.currentRecord);
-						console.log('==================================')
-						mapFields.write(this.backlog);
-						this.backlog = [];
-					}
-					this.backlog.push(chunk);
-				};
+// 					if (data[primaryKey] !== this.currentRecord) {
+// 						this.currentRecord = data[primaryKey];
+// 						console.log('Starting ' + this.currentRecord);
+// 						console.log('==================================')
+// 						mapFields.write(this.backlog);
+// 						this.backlog = [];
+// 					}
+// 					this.backlog.push(chunk);
+// 				};
 
 			
 
-				toXML = through (function write(data) {
-					var options = {declaration: {
-						include: false
-					}}
-					this.queue (json2xml('iati-activity', data))
-					this.queue('\n');
-				})
+// 				toXML = through (function write(data) {
+// 					var options = {declaration: {
+// 						include: false
+// 					}}
+// 					this.queue (json2xml('iati-activity', data))
+// 					this.queue('\n');
+// 				})
 
-		})();
-	}
+// 		})();
+// 	}
 
-}
-
-
-
-
-			var 
-
-			// output.write('<foo>');
-
-			input.pipe(parser).pipe(group).pipe(mapFields).pipe(toXML).pipe(output);
-
-			// output.write('</foo>');
-
-			// jsonData = _.groupBy(jsonData, function(d) {return d[tables.exampleTable.primaryKey]});
+// }
 
 
 
-			// _.keys(jsonData).forEach(function(key, i) {
+
+
+// 			// output.write('<foo>');
+
+// input.pipe(parser).pipe(group).pipe(mapFields).pipe(toXML).pipe(output);
+
+// 			// output.write('</foo>');
+
+// 			// jsonData = _.groupBy(jsonData, function(d) {return d[tables.exampleTable.primaryKey]});
+
+
+
+// 			// _.keys(jsonData).forEach(function(key, i) {
 				
-			// 	var query = {};
+// 			// 	var query = {};
 
-			// 	console.log('==========================================================================')
-			// 	console.log((i+1) + ' of ' + keys.length);
-			// 	console.log(key);
-			// 	console.log('==========================================================================')
+// 			// 	console.log('==========================================================================')
+// 			// 	console.log((i+1) + ' of ' + keys.length);
+// 			// 	console.log(key);
+// 			// 	console.log('==========================================================================')
 
 
-			// 	query[tables.exampleTable.primaryKey] = key;
+// 			// 	query[tables.exampleTable.primaryKey] = key;
 
-			// 	var data = _.clone(jsonData[key]),
-			// 		partialResult = _.clone(flatJsonToNested(data, tables.exampleTable));
+// 			// 	var data = _.clone(jsonData[key]),
+// 			// 		partialResult = _.clone(flatJsonToNested(data, tables.exampleTable));
 
-			// 	result.write(json2xml('foo', partialResult));
+// 			// 	result.write(json2xml('foo', partialResult));
 
-		// 	})
-		// }
-	// })
-}
+// 		// 	})
+// 		// }
+// 	// })
+// }
--- run.js
+++ run.js
@@ -1,83 +1,38 @@
-// var app = require('./../app.js');
+var fs = require('fs'),
+	Csv2Xml = require('./app.js');
 
 
-// // var csvData = {
-// // 	exampleTable: 'project id,title,sector name,sector code\r\n1,Example Project 1,Agriculture,A\r\n1,Example Project 1,Food Production,FP\r\n2,Example Project 2,Noise,N'
-// // }
+var tables = {
+	exampleTable: {
+		primaryKey:'project_ID',
+		mapping: {
+			'project_ID': ['iati-activity','iati-identifier','#'],
+			'transaction_values': ['iati-activity','transaction','value','#'],
+			'transaction_type_code': ['iati-activity','transaction','transaction-type','@','code'],
+			'transaction_type_name': ['iati-activity','transaction','transaction-type','#'],
+			'transaction_date': ['iati-activity','transaction','transaction-date','#'],
+			'project_title': ['iati-activity','title','#'],
+			'ad_sector_name': ['iati-activity','sector','#'],
+			'ad_sector_code': ['iati-activity','sector','@','code'],
+			'precision_code': ['iati-activity','location','coordinates','@','precision'],
+			'geoname_ID': ['iati-activity','location','gazetteer-entry','@','gazetteer-ref'],
+			'location_type': ['iati-activity','location','location-type','#'],
+			'location_code': ['iati-activity','location','location-type','@','code'],
+			'latitude': ['iati-activity','location','coordinates','@','latitude'],
+			'longitude': ['iati-activity','location','coordinates','@','longitude'],
+			'placename': ['iati-activity','location','name','#'],
+			'donors': ['iati-activity','participating-org','#'],
+			'iati_donor_codes': ['iati-activity','participating-org','@','ref'],
+			'status': ['iati-activity','activity-status','#']
 
-// var csvData = {};
-
-
-
-// var tables = {
-// 	exampleTable: {
-// 		primaryKey:'project_ID',
-// 		mapping: {
-// 			'project_ID': ['iati-activity','iati-identifier','#'],
-// 			'transaction_values': ['iati-activity','transaction','value','#'],
-// 			'transaction_type_code': ['iati-activity','transaction','transaction-type','@','code'],
-// 			'transaction_type_name': ['iati-activity','transaction','transaction-type','#'],
-// 			'transaction_date': ['iati-activity','transaction','transaction-date','#'],
-// 			'project_title': ['iati-activity','title','#'],
-// 			'ad_sector_name': ['iati-activity','sector','#'],
-// 			'ad_sector_code': ['iati-activity','sector','@','code'],
-// 			'precision_code': ['iati-activity','location','coordinates','@','precision'],
-// 			'geoname_ID': ['iati-activity','location','gazetteer-entry','@','gazetteer-ref'],
-// 			'location_type': ['iati-activity','location','location-type','#'],
-// 			'location_code': ['iati-activity','location','location-type','@','code'],
-// 			'latitude': ['iati-activity','location','coordinates','@','latitude'],
-// 			'longitude': ['iati-activity','location','coordinates','@','longitude'],
-// 			'placename': ['iati-activity','location','name','#'],
-// 			'donors': ['iati-activity','participating-org','#'],
-// 			'iati_donor_codes': ['iati-activity','participating-org','@','ref'],
-// 			'status': ['iati-activity','activity-status','#']
-
-// 		}
-// 	}
-// }
-
-// var fs = require('fs');
-
-// // csvData.exampleTable = fs.readFileSync('./test/data/np_join_split_huge.csv').toString();
-
-// // app(csvData, tables, function(err, res) {
-
-// // 	fs.writeFileSync('./result.xml', res)
-// // })
-
-// app(csvData, tables);
-
-
-// var output = fs.createWriteStream('./result.xml');
-// var input = fs.createReadStream('./test/data/np_join_split_noduplicates.csv');
-
-
-
-// var duplex1 = duplexer(through1, through2);
-
-var fs = require('fs'), 
-	file = fs.createReadStream('./test/data/np_join_split_noduplicates.csv'),
-	WritableStream = require('stream').Writable;
-
-
-var group = WritableStream();
-
-var mapFields = WritableStream();
-
-mapFields._write = function(chunk, enc, next) {
-	console.log('woooh', chunk);
-}
-
-group._write = function(chunk, enc, next) {
-	mapFields.write(chunk, enc);
-	next();
+		}
+	}
 }
 
 
+var test = new Csv2Xml(tables.exampleTable);
 
-file.on('error', function(err) {console.log(err)})
+var inFile = fs.createReadStream('./test/data/np_join_split.csv');
+var outFile = fs.createWriteStream('./result.xml');
 
-file.pipe(group)
-
-
-//input.pipe(parser).pipe(group).pipe(mapFields).pipe(toXML).pipe(output);
+inFile.pipe(test).pipe(outFile);
