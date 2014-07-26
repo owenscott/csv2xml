784b7cd4bdbd45b3b27cab58ae910b65c0213cc9
Owen Scott <owen.m.scott@gmail.com>
2014-07-26 16:34:37 -0400

    multipipe works

--- app.js
+++ app.js
@@ -20,64 +20,44 @@ var fs = require('fs'),
 
 module.exports = function Csv2Xml(conf) {
 
-	return through(function write (data) {
+	var self = this,
+		parser = csvParse({columns:true}),
+		group,
+		mapFields,
+		toXML;
+		// split = split();
 
-		var self = this,
-			parser = csvParse({columns:true}),
-			group = new WritableStream({ objectMode: true }),
-			mapFields = new WritableStream({ objectMode: true }),
-			toXML = new WritableStream({ objectMode: true });
-			// split = split();
+	//converts json to XML and writes to the parent function's queue (to be piped to wherever by the user)
+	toXML = through( function write (data) {
+		this.queue(json2xml('iati-activity', data))
+		this.queue('\n');	
+	});
 
-		//converts json to XML and writes to the parent function's queue (to be piped to wherever by the user)
-		toXML._write = function(chunk, enc, next) {
-			data = JSON.parse(chunk.toString());
-			self.queue(json2xml('iati-activity', data))
-			self.queue('\n');	
-			next();	
-		}
-
-		//takes flat JSON and converts to nested JSON according to the user-supplied configuration and writes to 'toXML'
-		mapFields._write = function(chunk, enc, next) {
-			var data = JSON.parse(chunk.toString());
-			toXML.write(JSON.stringify(flatJsonToNested(data, conf)), 'utf8')
-			next();
-		}
-
-		//takes the raw CSV stream and groups it into batches by primary key nad writes to 'mapFields'
-		group._write = function(chunk, enc, next) {
-
-			var data = JSON.parse(chunk.toString());
-
-			this.backlog = this.backlog || [];
-
-			if (data[conf.primaryKey] !== this.currentRecord && this.backlog.length) {
-				this.currentRecord = data[conf.primaryKey];
-				// console.log('Starting ' + this.currentRecord);
-				// console.log('==================================')
-				mapFields.write(JSON.stringify(_.clone(this.backlog)), 'utf8');
-				this.backlog = [];
-			}
-			else {
-				this.backlog.push(data);
-			}
-			next();
-
-		};
-
-		//parses the CSV text into flat json objects (one per row) and then writes to 'group'
-		parser.on('data', function(data) {
-			// console.log('parser');
-			group.write(JSON.stringify(data), 'utf8');
-		});
-
-		// split.on('data', function(data) {
-		// 	parser.write(data);
-		// });
-
-		// split.write(data);
-
-		parser.write(data);
-
+	//takes flat JSON and converts to nested JSON according to the user-supplied configuration and writes to 'toXML'
+	mapFields = through( function write (data) {
+		this.queue(flatJsonToNested(data, conf))
 	})
+
+	//takes the raw CSV stream and groups it into batches by primary key nad writes to 'mapFields'
+	group = through( function write(data) { 
+
+		this.backlog = this.backlog || [];
+
+		if (data[conf.primaryKey] !== this.currentRecord && this.backlog.length) {
+			this.currentRecord = data[conf.primaryKey];
+			// console.log('Starting ' + this.currentRecord);
+			// console.log('==================================')
+			this.queue(_.clone(this.backlog));
+			this.backlog = [];
+		}
+		else {
+			this.backlog.push(data);
+		}
+
+	});
+
+	//parses the CSV text into flat json objects (one per row) and then writes to 'group'
+
+	return multipipe(parser, group, mapFields, toXML);
+
 }
